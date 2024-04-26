9b613300d5035dc076955399516d3b8603bb0fb2
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2024-04-26 10:47:01 -0700

    fix: added transfer call

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,18 @@
 from ape import accounts, networks, project, Contract
 
 # Replace with the actual contract address after deployment
-CONTRACT_ADDRESS = "0x74bEf07e6EAeab5E44b54C5449717e203B718Abd"
+CONTRACT_ADDRESS = "0x4B3605c1f1089b33838538E6FabE5984100CB9ae"
 
 # The sender and receiver addresses
-SENDER_ADDRESS = accounts.load("sepDev")
+SENDER_ADDRESS = accounts.load("sepDev2")
 RECEIVER_ADDRESS = accounts.load("sepDev")
 
 # The amount to send (in wei)
-AMOUNT_TO_SEND = 100
+AMOUNT_TO_SEND = 10000
 
 def main():
     # Load the account of the sender
-    sender_account = accounts.load("sepDev")  # Use the correct alias for your account
+    sender_account = accounts.load("sepDev2")  # Use the correct alias for your account
 
     # Connect to the Sepolia network
     with networks.parse_network_choice("ethereum:sepolia") as provider:
@@ -25,10 +25,9 @@ def main():
         print(f"Sender balance before sending: {sender_balance / 1e18} eth")
 
         # Send ETH from the contract to the receiver
-        try: 
-            tx = my_contract.withdraw(RECEIVER_ADDRESS, str(AMOUNT_TO_SEND), sender=sender_account)
-            receipt = tx.wait_for_receipt()
-            print(f"Transaction receipt: {receipt}")
+        try:
+            sender_account.transfer(my_contract, ".001 ether",gas_limit=50_000) 
+            tx = my_contract.withdraw(sender=sender_account)
         except Exception as e:
             print(e)
 
