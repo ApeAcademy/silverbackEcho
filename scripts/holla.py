e01d4a96b146af8504070bcab4f11c8f4b8d96e9
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2024-06-05 12:11:50 -0700

    fix: readme with suggestions

--- scripts/holla.py
+++ scripts/holla.py
@@ -2,15 +2,10 @@ from ape import accounts, networks, Contract
 
 # Replace with the actual contract address after deployment
 CONTRACT_ADDRESS = "0xE8116A0Fb2D4Ee04F570fbEA4460F9C7B0121D76"
-
 # The amount to send (in wei)
 AMOUNT_TO_SEND = 10000000000000000 # .01 eth
 
-
-with networks.parse_network_choice("ethereum:sepolia"):
-    # Load the contract
-    my_contract = Contract(CONTRACT_ADDRESS)
-
+my_contract = Contract(CONTRACT_ADDRESS)
 sender_account = accounts.load("sepDev")  # Use the correct alias for your account
 
 def main():
