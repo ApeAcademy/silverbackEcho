fb6b007206e3172e94baf7c26c9428e2fc0d5a5a
johnson2427 <blakeejohnson39@gmail.com>
2024-06-14 14:21:39 -0500

    refactor: some cleanup before adding dockerfile

--- bots/hollaback.py
+++ bots/hollaback.py
@@ -10,27 +10,28 @@ import requests
 
 from wonderwords import RandomWord
 
-STABILITY_KEY= os.environ.get("STABILITY_KEY")
-PINATA_API_KEY= os.environ.get("PINATA_API_KEY")
-PINATA_SECRET_API_KEY= os.environ.get("PINATA_SECRET_API_KEY")
+STABILITY_KEY = os.environ.get("STABILITY_KEY")
+PINATA_API_KEY = os.environ.get("PINATA_API_KEY")
+PINATA_SECRET_API_KEY = os.environ.get("PINATA_SECRET_API_KEY")
 
 app = SilverbackApp()
 warper = accounts.load("warpNinjagod")
 client = Warpcast(warper)
 my_contract = project.Echo.at(os.environ.get("ECHO_CONTRACT"))
 
-def create_prompt(number_adj:int) -> str:
+
+def create_prompt(number_adj: int) -> str:
     w = RandomWord()
     adjectives = w.random_words(number_adj, include_categories=["adjective"])
     adjectives_string = ", ".join(adjectives)
     prompt = f"An {adjectives_string} ape that is screaming AHHHHH."
-    
     return prompt
-    
+
+
 @app.on_(my_contract.Received)
 def payment_received(log):
     print(log)
-    prompt = create_prompt(max(floor(log10(log.amount))- 12, 1))
+    prompt = create_prompt(max(floor(log10(log.amount)) - 12, 1))
     for _ in range(10):
         print(f"\n{prompt}\n")
         # create image from AI using scream
@@ -39,8 +40,7 @@ def payment_received(log):
             break
         except Exception as e:
             print(e)
-            prompt = create_prompt(max(floor(log10(log.amount))- 12, 1))
-        
+            prompt = create_prompt(max(floor(log10(log.amount)) - 12, 1))
 
     # open the created image
     print(fileName + " has beeen created")
@@ -49,42 +49,40 @@ def payment_received(log):
     ipfsHash = uploadToPinata(image)
     # cast the message with the scream and ipfs link
     ipfsLink = "https://jade-slow-pigeon-687.mypinata.cloud/ipfs/" + ipfsHash
-    warpcast_text =  f"A picture of {log.sender}:\n"
+    warpcast_text = f"A picture of {log.sender}:\n"
     client.post_cast(text=warpcast_text, embeds=[ipfsLink])
 
 
 def createImage(prompt):
-    
     response = requests.post(
-    "https://api.stability.ai/v2beta/stable-image/generate/ultra",
-    headers={
-        "authorization": STABILITY_KEY,
-        "accept": "image/*"
-    },
-    files={"none": ''},
-    data={
-        "prompt": prompt,
-        "output_format": "jpeg",
-   },
-)
-
+        "https://api.stability.ai/v2beta/stable-image/generate/ultra",
+        headers={
+            "authorization": STABILITY_KEY,
+            "accept": "image/*"
+        },
+        files={"none": ''},
+        data={
+            "prompt": prompt,
+            "output_format": "jpeg",
+        },
+    )
     if response.status_code == 200:
         with tempfile.NamedTemporaryFile(delete=False, suffix=".jpeg") as fp:
             fp.write(response.content)
     else:
         raise Exception(str(response.json()))
-        
     return fp.name
+
 
 def uploadToPinata(image):
     url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-
     headers = {
-    'pinata_api_key': PINATA_API_KEY,
-    'pinata_secret_api_key': PINATA_SECRET_API_KEY,}
+        'pinata_api_key': PINATA_API_KEY,
+        'pinata_secret_api_key': PINATA_SECRET_API_KEY,
+    }
 
     files = {'file': ("ApeScream", image)}
     response = requests.request("POST", url, files=files, headers=headers)
-    
     response.raise_for_status()
     return response.json()["IpfsHash"]
+
--- scripts/holla.py
+++ scripts/holla.py
@@ -4,39 +4,49 @@ import os
 CONTRACT_ADDRESS = os.environ.get("ECHO_CONTRACT")
 
 # The amount to send (in wei)
-AMOUNT_TO_SEND = 10000000000000000 # .01 eth
+AMOUNT_TO_SEND = 10000000000000000  # Note: .01 eth
 
 
 with networks.parse_network_choice("ethereum:sepolia"):
     # Load the contract
     my_contract = Contract(CONTRACT_ADDRESS)
 
-sender_account = accounts.load("sepDev")  # Use the correct alias for your account
+# Note: Use the correct alias for your account
+receiver_account = accounts.load("sepDev2")
+# Note: Use the correct alias for your account
+sender_account = accounts.load("sepDev")
+
 
 def main():
-    
     sender_balance = sender_account.balance
     print(f"Sender balance before sending: {sender_balance / 1e18} eth")
 
     # Send ETH from the contract to the receiver
     try:
         # transfer eth to contract
-        tx_transfer = sender_account.transfer(my_contract, "0.001 ether",gas_limit=100_000)
+        tx_transfer = sender_account.transfer(
+            my_contract,
+            "0.001 ether",
+            gas_limit=100_000,
+        )
         print(tx_transfer)
     except Exception as e:
         print(e)
 
-receiver_account = accounts.load("sepDev2")  # Use the correct alias for your account
 
 def withdraw_contract_balance():
     try:
         # withdraw money from contract
         print("Echo contract balance before withdraw: " + my_contract.balance)
-        tx_withdraw = my_contract.withdraw(sender=receiver_account, gas_limit=100_000)
+        tx_withdraw = my_contract.withdraw(
+            sender=receiver_account, gas_limit=100_000
+        )
         print(tx_withdraw)
         print("Echo contract balance after withdraw: " + my_contract.balance)
     except Exception as e:
         print(e)
 
+
 if __name__ == "__main__":
     main()
+
